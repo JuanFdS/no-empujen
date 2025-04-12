06959ff3de971fcf8db3be28b877699a99d1bcf7
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2025-04-12 18:03:57 -0300

    Entregado para la jam con -3 minutos de sobra

--- godot/scenes/elementos_del_juego/avanzar.gd
+++ godot/scenes/elementos_del_juego/avanzar.gd
@@ -3,4 +3,5 @@ extends Button
 func _pressed() -> void:
 	await %FadeOverlay.fade_in()
 	var next_scene = %ReglasDelJuego.next_scene
-	get_tree().change_scene_to_file(next_scene)
+	if next_scene:
+		get_tree().change_scene_to_file(next_scene)
