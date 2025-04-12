afffb2219bee5ea89df1fec4e582f9442bc79b61
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2025-04-11 22:00:07 -0300

    Jugable

--- godot/scenes/niveles/reglas_del_juego.gd
+++ godot/scenes/niveles/reglas_del_juego.gd
@@ -3,8 +3,15 @@ extends Node2D
 const VELOCIDAD_DE_PIEZA = 1000
 var pieza_siendo_arrastrada: RigidBody2D = null
 var punto_de_ancla: Vector2 = Vector2.ZERO
+@onready var salida: Area2D = %Salida
+@export var pieza_objetivo: RigidBody2D
+var resuelto: bool = false
 
 func _ready() -> void:
+	salida.body_entered.connect(func(body):
+		if body == pieza_objetivo:
+			ganaste()
+	)
 	for pieza in get_tree().get_nodes_in_group("pieza"):
 		pieza.freeze = true
 		pieza.input_event.connect(self.on_pieza_input_event.bind(pieza))
@@ -38,11 +45,17 @@ func dejar_de_arrastrar(pieza):
 	pieza_siendo_arrastrada = null
 
 func on_pieza_input_event(viewport: Node, event: InputEvent, shape_idx: int, pieza: RigidBody2D):
+	if resuelto:
+		return
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
 			empezar_a_arrastrar(pieza)
 
 func _physics_process(delta: float) -> void:
+	if resuelto:
+		pieza_objetivo.freeze = false
+		pieza_objetivo.linear_velocity = Vector2.LEFT * VELOCIDAD_DE_PIEZA
+		return
 	if pieza_siendo_arrastrada:
 		var posicion_origen = pieza_siendo_arrastrada.global_position
 		var posicion_destino = get_global_mouse_position() - punto_de_ancla
@@ -52,6 +65,13 @@ func _physics_process(delta: float) -> void:
 			pieza_siendo_arrastrada.linear_velocity = Vector2.ZERO
 
 func _input(event: InputEvent) -> void:
+	if resuelto:
+		return
 	if event is InputEventMouseButton and pieza_siendo_arrastrada:
 		if event.button_index == MOUSE_BUTTON_LEFT and not event.is_pressed():
 			dejar_de_arrastrar(pieza_siendo_arrastrada)
+
+func ganaste():
+	dejar_de_arrastrar.call_deferred(pieza_siendo_arrastrada)
+	resuelto = true
+	$"../UI/Ganaste/AnimationPlayer".play("ganaste")
